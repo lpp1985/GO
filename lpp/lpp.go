396b408a9cf79b907ba1bc8d70821e451c88ae4a
lpp <lpp1985@hotail.com>
2016-12-01 12:07:29 +0800

    123

--- lpp/lpp.go
+++ lpp/lpp.go
@@ -3,7 +3,7 @@ package lpp
 
 import (
 	"bufio"
-
+	"bytes"
 	"io"
 	"os"
 )
@@ -14,10 +14,9 @@ type Block_Reading struct {
 	Buffer   int
 }
 type IO struct {
-	Io          *bufio.Reader
-	BlockTag    []byte
-	SplitTag    byte
-	BlockString string
+	Io       *bufio.Reader
+	BlockTag []byte
+	SplitTag byte
 }
 
 func (blockreading *Block_Reading) Read() (IO, error) {
@@ -25,7 +24,7 @@ func (blockreading *Block_Reading) Read() (IO, error) {
 
 	raw_file, err := os.Open(blockreading.File)
 	if blockreading.Buffer == 0 {
-		blockreading.Buffer = 99999999999
+		blockreading.Buffer = 9999999999
 	}
 	BlockIO.Io = bufio.NewReaderSize(raw_file, blockreading.Buffer)
 	if blockreading.Blocktag == "" {
@@ -34,7 +33,7 @@ func (blockreading *Block_Reading) Read() (IO, error) {
 		BlockIO.BlockTag = []byte(blockreading.Blocktag)
 	}
 	BlockIO.SplitTag = byte([]byte(blockreading.Blocktag)[len(blockreading.Blocktag)-1])
-	BlockIO.BlockString = blockreading.Blocktag
+
 	return BlockIO, err
 
 }
@@ -54,7 +53,7 @@ func (Reader IO) Next() ([]byte, error) {
 			}
 
 			if len(Reader.BlockTag) > 1 {
-				if len(out_tag) >= len(Reader.BlockTag) && string(out_tag[(len(out_tag)-len(Reader.BlockTag)):]) == Reader.BlockString {
+				if len(out_tag) >= len(Reader.BlockTag) && bytes.Equal(out_tag[(len(out_tag)-len(Reader.BlockTag)):], Reader.BlockTag) {
 
 					break
 				}
