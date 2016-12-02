df84fe5476a33c367f65d5a7ac82e1be4913b16f
lpp <lpp1985@hotail.com>
2016-12-03 00:14:34 +0800

    123

--- lpp/lpp.go
+++ lpp/lpp.go
@@ -4,9 +4,78 @@ package lpp
 import (
 	"bufio"
 	"bytes"
+	//	"fmt"
 	"io"
 	"os"
+	"sort"
 )
+
+type File_Ddict struct {
+	File_IO IO
+	Header  bool
+}
+
+func (file *File_Ddict) Read(key int, value int) map[string]map[string]string {
+	key--
+	value--
+	var result_hash map[string]map[string]string = make(map[string]map[string]string)
+	if file.Header == true {
+		file.File_IO.Next()
+	}
+
+	for {
+
+		line, err := file.File_IO.Next()
+		if err != nil {
+			break
+		}
+		line_l := bytes.Split(bytes.TrimSpace(line), []byte("\t"))
+
+		if len(line_l) > sort.IntSlice([]int{key, value})[0] {
+			key_string := string(line_l[key])
+			value_string := string(line_l[value])
+			_, ok := result_hash[key_string][value_string]
+
+			if !ok {
+				result_hash[key_string] = make(map[string]string)
+				result_hash[key_string][value_string] = ""
+
+			}
+		}
+
+	}
+	return result_hash
+}
+
+type File_dict struct {
+	File_IO IO
+	Header  bool
+}
+
+func (file *File_dict) Read(key int, value int) map[string]string {
+	key--
+	value--
+	var result_hash map[string]string = make(map[string]string)
+	if file.Header == true {
+		file.File_IO.Next()
+	}
+	for {
+
+		line, err := file.File_IO.Next()
+		if err != nil {
+			break
+		}
+		line_l := bytes.Split(bytes.TrimSpace(line), []byte("\t"))
+		if len(line_l) > sort.IntSlice([]int{key, value})[0] {
+			key_string := string(line_l[key])
+			value_string := string(line_l[value])
+			result_hash[key_string] = value_string
+
+		}
+
+	}
+	return result_hash
+}
 
 type Block_Reading struct {
 	File     string
@@ -24,7 +93,7 @@ func (blockreading *Block_Reading) Read() (IO, error) {
 
 	raw_file, err := os.Open(blockreading.File)
 	if blockreading.Buffer == 0 {
-		blockreading.Buffer = 9999999999
+		blockreading.Buffer = 99999999
 	}
 	BlockIO.Io = bufio.NewReaderSize(raw_file, blockreading.Buffer)
 	if blockreading.Blocktag == "" {
@@ -45,6 +114,7 @@ func (Reader IO) Next() ([]byte, error) {
 
 	for {
 		line, err := Reader.Io.ReadSlice(Reader.SplitTag)
+
 		if err == nil {
 			if len(out_tag) > 1 {
 				out_tag = append(out_tag, line...)
@@ -63,11 +133,11 @@ func (Reader IO) Next() ([]byte, error) {
 			}
 
 		} else if err == bufio.ErrBufferFull || err == io.EOF {
-			out_tag = append(out_tag, line...)
 			if err == io.EOF {
 				status = err
 				break
 			}
+			out_tag = append(out_tag, line...)
 
 		}
 
